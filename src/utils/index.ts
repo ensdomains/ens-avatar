654587155698110602b383776fcc35b2a4d3f9eb
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2023-05-09 03:30:20 +0200

    export missing utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,17 +1,22 @@
+import { assert } from './assert';
 import { handleSettled } from './handleSettled';
 import { parseNFT } from './parseNFT';
 import { BaseError } from './error';
-import { getImageURI } from './getImageURI';
+import { convertToRawSVG, getImageURI } from './getImageURI';
 import { resolveURI } from './resolveURI';
 import { createCacheAdapter, fetch } from './fetch';
+import { isCID } from './isCID';
 import { isImageURI } from './isImageURI';
 
 export {
   BaseError,
+  assert,
+  convertToRawSVG,
   createCacheAdapter,
   fetch,
   getImageURI,
   handleSettled,
+  isCID,
   isImageURI,
   parseNFT,
   resolveURI,
