2d52a5c6a86c0226b139c6d7219da6d0d714727b
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2023-06-15 01:25:45 +0200

    bash linux compatibility

--- patch.sh
+++ patch.sh
@@ -1,9 +1,22 @@
+#!/bin/bash
+
 # patch for: https://github.com/jaredpalmer/tsdx/issues/667
 # patch for: https://github.com/jaredpalmer/tsdx/issues/276#issuecomment-1143821119
 
+OS=$(uname)
+
+if [[ "$OS" == "Darwin" ]]; then # macOS
+  SEDCMD="sed -i ''"
+elif [[ "$OS" == "Linux" ]]; then
+  SEDCMD="sed -i"
+else
+  echo "Unsupported OS"
+  exit 1
+fi
+
 modify_imports() {
-  sed -i '' 's/\.\.\/\.\.\/_virtual/\.\.\/_virtual/g' "$1"
-  sed -i '' 's/\.\.\/\.\.\/node_modules/\.\.\/node_modules/g' "$1"
+  $SEDCMD -i '' 's/\.\.\/\.\.\/_virtual/\.\.\/_virtual/g' "$1"
+  $SEDCMD -i '' 's/\.\.\/\.\.\/node_modules/\.\.\/node_modules/g' "$1"
 }
 
 files=("dist/specs/*.esm.js" "dist/utils/*.esm.js")
@@ -17,5 +30,5 @@ do
 done
 
 # dist/index.esm.js replaced seperately
-sed -i '' 's/\.\.\/_virtual/\.\/_virtual/g' "dist/index.esm.js"
-sed -i '' 's/\.\.\/node_modules/\.\/node_modules/g' "dist/index.esm.js"
+$SEDCMD -i '' 's/\.\.\/_virtual/\.\/_virtual/g' "dist/index.esm.js"
+$SEDCMD -i '' 's/\.\.\/node_modules/\.\/node_modules/g' "dist/index.esm.js"
